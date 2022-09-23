f16ec9fcdd3c2bf823c17b247b06c2753ce501c7
tungUAS <tung.tt133@gmail.com>
2022-09-23 16:34:28 +0200

    add note

--- Frontend/src/app/services/crawl.service.ts
+++ Frontend/src/app/services/crawl.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, Observable } from 'rxjs';
 import { prof } from '../models/prof.model';
-import { Uhr, Uhren } from '../models/uhr.model';
+import { Uhr, Uhren, uhrUpdate } from '../models/uhr.model';
 import { ConfigService } from './config.service';
+import { stringToKeyValue } from '@angular/flex-layout/extended/style/style-transforms';
 
 @Injectable({
   providedIn: 'root'
@@ -21,7 +22,19 @@ export class CrawlService {
     return this.http.get<Uhren>('http://localhost:3000/uhren/get');
   }
 
-  saveUhrIntoDB(uhr:Uhr):Observable<Uhr>{
-    return this.http.post<Uhr>('http://localhost:3000/uhren/save',uhr).pipe();
+  // cant use pipe
+  // why ?
+  updateUhrDB(update:uhrUpdate){
+    console.log("update Uhr DB fn ",update);
+    return this.http.put<uhrUpdate>('http://localhost:3000/uhren/update',update).subscribe(
+      data=>{
+        console.log(data);
+      }
+    );
   }
+
+/*   saveUhrIntoDB(uhr:Uhr):Observable<Uhr>{
+    return this.http.post<Uhr>('http://localhost:3000/uhren/save',uhr).pipe();
+  } */
 }
+
